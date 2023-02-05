c084f5e21d57ccd162af255b04836a05ccce6934
Isaaccna <isaac.andrade1231@gmail.com>
2023-02-05 02:11:44 +0000

    done

--- src/main/java/SetAllArrayElements.java
+++ src/main/java/SetAllArrayElements.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 
 public class SetAllArrayElements {
     /**
@@ -23,6 +24,8 @@ public class SetAllArrayElements {
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
     public void set(int[] arr, int n){
-
+     for (int i = 0; i < arr.length; i++) {
+        Array.set(arr, i, n);
+     }
     }
 }
